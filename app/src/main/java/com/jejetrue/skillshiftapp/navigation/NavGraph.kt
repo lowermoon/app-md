a61eeed04dad036269ac9370ad5abc8c81c411b0
JejeTrue <jejedanjihaan@gmail.com>
2023-11-16 00:06:31 +0700

    mengubah button,mengganti dan menambah TextField, menambah Navigasi, menambah halaman Home dan Verify

---
+++ app/src/main/java/com/jejetrue/skillshiftapp/navigation/NavGraph.kt
@@ -0,0 +1,2 @@
+package com.jejetrue.skillshiftapp.navigation
+
--- app/src/main/java/com/jejetrue/skillshiftapp/view/MainActivity.kt
+++ app/src/main/java/com/jejetrue/skillshiftapp/view/MainActivity.kt
@@ -10,8 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.jejetrue.skillshiftapp.screens.LoginScreen
-import com.jejetrue.skillshiftapp.screens.SingupScreen
+import com.jejetrue.skillshiftapp.view.LoginScreen
 import com.jejetrue.skillshiftapp.ui.theme.SkillShiftAppTheme
 
 class MainActivity : ComponentActivity() {
